24521c2a0dc9a6b5842cd1305d2f31aedff17d88
dretoba91e <64743870+dretoba91@users.noreply.github.com>
2021-11-27 10:38:07 +0100

    created state for the search inputs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
+import React, { useState } from "react";
 import "./App.css";
 import Bnblogo from "./component/Bnblogo";
 import SearchBar from "./component/SearchBar";
+import BnbInfo from "./component/BnbInfo.json";
 
 function App() {
+  // 1. create state to filter the data and show only the ones matching the user search.
+  const [locationFiltered, setLocationFiltered] = useState("");
+  const [totalGuest, setTotalGuest] = useState(0);
+
+  // 2. create a State for the data filtering that is condition to seacrch button when clicked.
+  const [dataArray, setDataArray] = useState(BnbInfo);
+
+  // 3. need to format user search for (, and " ") using regex.
+  const re = /\s*,\s*/;
+  const result = locationFiltered.includes(",")
+    ? locationFiltered.trim().split(re)
+    : locationFiltered.trim().replace(/\s\s+/g).split(" ");
+  console.log(result);
+
+  // 4. create a new array of the filtered object from the JSON array using the filtered user search
+  const filteredBnbList = BnbInfo.filter((data) => {
+    if (totalGuest <= data.maxGuests) {
+      if (result.length <= 1) {
+        if (locationFiltered === "") {
+          return data;
+        } else if (
+          data.city.toLowerCase().includes(result[0].toLowerCase()) ||
+          data.country.toLowerCase().includes(result[0].toLowerCase())
+        ) {
+          return data;
+        }
+      } else if (result.length > 1) {
+        if (locationFiltered === "") {
+          return data;
+        } else if (
+          data.city.toLowerCase().includes(result[0].toLowerCase()) &&
+          data.country.toLowerCase().includes(result[1].toLowerCase())
+        ) {
+          return data;
+        } else if (
+          data.city.toLowerCase().includes(result[1].toLowerCase()) &&
+          data.country.toLowerCase().includes(result[0].toLowerCase())
+        ) {
+          return data;
+        }
+      }
+    }
+    return false;
+  });
+
   return (
     <div className="App">
       <div className="App-header">
         <Bnblogo />
-        <SearchBar />
+        <SearchBar
+          filteredBnbList={filteredBnbList}
+          locationFiltered={locationFiltered}
+          setLocationFiltered={setLocationFiltered}
+          setDataArray={setDataArray}
+          totalGuest={totalGuest}
+          setTotalGuest={setTotalGuest}
+        />
       </div>
     </div>
   );
--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import GuestSearch from "./GuestSearch";
 import LocationSearch from "./LocationSearch";
 import styles from "./SearchBar.module.css";
+import { FaSearch } from "react-icons/fa";
+import { AiFillCloseCircle } from "react-icons/ai";
 
-function SearchBar() {
+// destructuring props from App.js as parameter for SearchBar
+function SearchBar({
+  filteredBnbList,
+  locationFiltered,
+  setLocationFiltered,
+  setDataArray,
+  totalGuest,
+  setTotalGuest,
+}) {
+  // 1. create a state to check the input tag on focus between the loction or guest.
+  const [inputOnFocus, setInputOnFocus] = useState("");
+
+  // 2. create a state for the search dropdown when to open and close.
+  const [showSearch, setShowSearch] = useState(false);
+
+  // 2a. make a condition for when the search drodown should open and close.
+  const searchDropdown = showSearch ? "openDropdown" : "closeDropdown";
+
+  // 2b. create function to open and close the search dropdown
+  const openSearch = () => {
+    setShowSearch(true);
+  };
+
+  const closeSearch = () => {
+    setShowSearch(false);
+  };
+
+  //3. when dropdown is open, show search dropdown header
+  const searchDropdownHeader = showSearch && (
+    <div className={searchDropdownHeader}>
+      <div className="searchDropdownIcon" onClick={closeSearch}>
+        <AiFillCloseCircle />
+      </div>
+    </div>
+  );
+
+  // 4. create a state for the Guest filter (both adult and child), to count the number of guest.
+  const [adultCount, setAdultCount] = useState(0);
+  const [childCount, setChildCount] = useState(0);
+
+  // 5a. create an event handler for search button
+  //the onclick event handler will toggle the search button and also send filtered list to the CardDisplay component through the dataArray props.
+  const handleSearchButton = (e) => {
+    setTotalGuest(adultCount + childCount);
+    setDataArray(filteredBnbList);
+    if (showSearch) {
+      setShowSearch(!showSearch);
+    }
+  };
+
+  // 5b.
+
   return (
     <div className={styles.searchbar}>
       <div className={styles.inputBar}>
@@ -12,7 +65,9 @@ function SearchBar() {
       <div className={styles.inputBar}>
         <GuestSearch />
       </div>
-      <button>Search</button>
+      <div className={styles.iconSearch}>
+        <FaSearch className={styles.fasearch} />
+      </div>
     </div>
   );
 }
